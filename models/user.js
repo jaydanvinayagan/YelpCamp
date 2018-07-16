d0d656f704863fed563c2e838788d1b16afc2f1e
Jaydan <jvinayagan@gmail.com>
2018-07-16 05:15:01 +0000

    added user profiles

--- models/user.js
+++ models/user.js
@@ -4,6 +4,10 @@ var passportLocalMongoose = require("passport-local-mongoose");
 var UserSchema = new mongoose.Schema({
    username: String,
    password: String,
+   firstName: String,
+   lastName: String,
+   email: String,
+   avatar: String,
    isAdmin: {type: Boolean, default: false}
  
    
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 var passport = require("passport");
 var User = require("../models/user");
-
+var Campground = require("../models/campground");
 
 //root router
 router.get("/",function(req,res){
@@ -21,7 +21,15 @@ router.get("/register", function(req, res) {
 // handle sign up logic
 router.post("/register", function(req, res) {
     
-    var newUser = new User({username: req.body.username});
+    var newUser = new User({
+        username: req.body.username,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        avatar: req.body.avatar
+    });
+    
+    
     
     if(req.body.adminCode === "secretCode"){  // admin code
         newUser.isAdmin = true;
@@ -58,6 +66,23 @@ router.get("/logout", function(req, res) {
    req.logout(); 
    req.flash("success", "Logged you out!");
    res.redirect("/campgrounds");
+});
+
+// USERS PROFILES
+router.get("/users/:id", function(req, res) {
+   User.findById(req.params.id,function(err, foundUser){
+       if(err){
+           req.flash("error","Something went wrong.");
+           res.redirect("/");
+       } 
+       Campground.find().where('author.id').equals(foundUser._id).exec(function(err, campgrounds ){
+           if(err){
+               req.flash("error","Something went wrong.");
+               res.redirect("/");
+            }
+            res.render("users/show", {user: foundUser, campgrounds: campgrounds});
+       });
+   })
 });
 
 function isLoggedIn(req, res, next){
